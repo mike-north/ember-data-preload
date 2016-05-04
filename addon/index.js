07688d877058ed228ffb776b927138fc2ad1ed8d
César Izurieta <cesar@caih.org>
2016-05-04 12:40:32 -0300

    Refactor preload to handle more cases

--- addon/index.js
+++ addon/index.js
@@ -2,41 +2,35 @@ import Ember from 'ember';
 
 const { RSVP } = Ember;
 
+function getPromise(object, property) {
+  return RSVP.resolve(Ember.get(object, property));
+}
+
 function preloadRecord(record, toPreload) {
-  return preloadAll([record], toPreload).then(() => {
-    return record;
-  });
+  if (!record) {
+    return RSVP.resolve(record);
+  }
+
+  switch(Ember.typeOf(toPreload)) {
+    case 'string':
+      return getPromise(record, toPreload).then(() => record);
+    case 'array':
+      return RSVP.all(toPreload.map((p) => preloadRecord(record, p))).then(() => record);
+    case 'object':
+      return RSVP.all(Object.keys(toPreload).map((p) =>
+        getPromise(record, p).then((data) => preload(data, toPreload[p])))).then(() => record);
+    default: throw 'Illegal Argument';
+  }
 }
 
 function preloadAll(records, toPreload) {
-    switch(Ember.typeOf(toPreload)) {
-      case 'object':
-        const properties = Object.keys(toPreload);
-        return RSVP.all(properties.map((p) => {
-          return RSVP.all(records.map((record) => {
-            return record.get(p);
-          })).then((data) => {
-            const subRecords = data.reduce((prev, cur) => prev.concat(cur.toArray()), []);
-            return preloadAll(subRecords, toPreload[p]);
-          });
-        })).then(() => records);
-      case 'string':
-        return RSVP.all(records.map((record) => record.get(toPreload)))
-          .then(() => records);
-      default: throw 'Illegal Argument';
-    }
+  return RSVP.all(records.map((record) => preload(record, toPreload)));
 }
 
 function preload(thing, toPreload) {
-  if (thing.then) {
-    return thing.then(() => {
-      return Ember.isArray(thing) ? preloadAll(thing, toPreload) : preloadRecord(thing, toPreload);
-    });
-  }
-  else {
+  return RSVP.resolve(thing).then(() => {
     return Ember.isArray(thing) ? preloadAll(thing, toPreload) : preloadRecord(thing, toPreload);
-  }
-
+  });
 }
 
 export default preload;
